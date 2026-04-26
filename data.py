872c969643c311596b04944129e3b1d872a5f4dd
Ramzi logistique <adelbouk55@gmail.com>
2026-04-26 12:48:01 +0200

    Add suppliers data and get_suppliers function

---
+++ data.py
@@ -0,0 +1,8 @@
+suppliers = [
+    {"id": 1, "name": "Supplier A", "location": "Algeria", "score": 85},
+    {"id": 2, "name": "Supplier B", "location": "France", "score": 90},
+    {"id": 3, "name": "Supplier C", "location": "China", "score": 78},
+]
+
+def get_suppliers():
+    return suppliers
